31b5e669e999e2f57d561a10a2cf4bb7b1fa7c9e
Krzysztof Gurniak <kgurniak91@gmail.com>
2025-10-14 19:40:54 +0200

    Closing the lookup window with ESC keyboard shortcut

--- subtitles-lookup-preload.js
+++ subtitles-lookup-preload.js
@@ -11,13 +11,20 @@ document.addEventListener('contextmenu', (e) => {
   }
 });
 
-// Keyboard Shortcut (Ctrl+Shift+S)
+// Keyboard Shortcut listener for events within the loaded website
 document.addEventListener('keydown', (e) => {
+  // Handle Ctrl+Shift+S for adding notes
   if (e.ctrlKey && e.shiftKey && e.key === 'S') {
     e.preventDefault();
     const selectedText = window.getSelection().toString().trim();
     if (selectedText) {
       ipcRenderer.send('lookup:add-note', {text: selectedText});
     }
+  }
+
+  // Handle Escape key to close the lookup window
+  if (e.key === 'Escape') {
+    e.preventDefault();
+    ipcRenderer.send('lookup:close-window');
   }
 });
