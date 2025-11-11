b977f753f4ff14d3372ad6199ad48762bf0fa846
Krzysztof Gurniak <kgurniak91@gmail.com>
2025-11-11 16:36:23 +0100

    Fixed inconsistent PrimeNG theme / color palette

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import {ConfirmationService, MessageService} from 'primeng/api';
 import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
 import {DialogService} from 'primeng/dynamicdialog';
 import {AppStateService} from './state/app/app-state.service';
+import {definePreset} from '@primeng/themes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +18,26 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Nora
+        preset: definePreset(Nora, {
+          semantic: {
+            primary: {
+              50: '{blue.50}',
+              100: '{blue.100}',
+              200: '{blue.200}',
+              300: '{blue.300}',
+              400: '{blue.400}',
+              500: '{blue.500}',
+              600: '{blue.600}',
+              700: '{blue.700}',
+              800: '{blue.800}',
+              900: '{blue.900}',
+              950: '{blue.950}'
+            }
+          }
+        }),
+        options: {
+          darkModeSelector: false
+        }
       },
       ripple: true
     }),
